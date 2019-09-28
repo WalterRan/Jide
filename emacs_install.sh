079cada86d36095e3c0a45d9e8d37025d8cdd40d
root <root@localhost.localdomain>
2019-09-28 12:03:02 -0400

    Add emacs install script

---
+++ emacs_install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+yum -y install libXpm-devel libjpeg-turbo-devel openjpeg-devel openjpeg2-devel turbojpeg-devel giflib-devel libtiff-devel gnutls-devel libxml2-devel GConf2-devel dbus-devel wxGTK-devel gtk3-devel libselinux-devel gpm-devel librsvg2-devel ImageMagick-devel libncurses-dev ncurses-devel gcc
+
+pushd
+
+cd /tmp
+
+curl -O http://ftp.gnu.org/gnu/emacs/emacs-25.1.tar.gz
+
+tar -zxvf emacs-25.1.tar.gz
+
+cd emacs-25.1
+
+./configure
+
+make
+
+make install
+
+popd
