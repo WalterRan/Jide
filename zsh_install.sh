3a8073a7919c5916a045f7c247cf928bc215fe14
root <root@localhost.localdomain>
2019-10-09 05:22:35 -0400

    Zsh replace 'ln -sf' by 'cp -ri'

--- zsh_install.sh
+++ zsh_install.sh
@@ -12,11 +12,12 @@ sh /tmp/install.sh
 
 rm ~/.zshrc -rf
 
-ln -sf `pwd`/zsh/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-ln -sf `pwd`/zsh/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-ln -sf `pwd`/zsh/autojump ~/.oh-my-zsh/custom/plugins/autojump
+# Replace 'ln -sf' by 'cp -ri'
+cp -ri `pwd`/zsh/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+cp -ri `pwd`/zsh/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+cp -ri `pwd`/zsh/autojump ~/.oh-my-zsh/custom/plugins/autojump
 
-ln -s $(realpath zsh/zshrc) ~/.zshrc
+cp -ri $(realpath zsh/zshrc) ~/.zshrc
 
 cd zsh/autojump
 ./install.py
