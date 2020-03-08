eb22ddf130026d1d7cf46ebb6542760a127c9b11
root <root@localhost.localdomain>
2020-03-08 16:53:41 -0400

    Fix #6 remove the things doesn't support well in Windows

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,8 @@
+(setq url-proxy-services
+   '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+     ("http" . "10.144.1.11:8080")
+     ("https" . "10.144.1.11:8080")))
+
 (require 'package)
 (add-to-list 'package-archives
          '("melpa" . "http://melpa.org/packages/") t)
@@ -20,10 +25,10 @@
 (add-to-list 'load-path "~/.emacs.d/custom")
 
 ;; (require 'setup-general)
-(if (version< emacs-version "24.4")
-    (require 'setup-ivy-counsel)
-  (require 'setup-helm)
-  (require 'setup-helm-gtags))
+;; (if (version< emacs-version "24.4")
+;;     (require 'setup-ivy-counsel)
+;;   (require 'setup-helm)
+;;   (require 'setup-helm-gtags))
 ;; (require 'setup-ggtags)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
@@ -33,17 +38,3 @@
 ;;  :ensure t
 ;;  :bind ("M-b" . avy-goto-char))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (go-mode lush-theme iedit anzu comment-dwim-2 ws-butler dtrt-indent clean-aindent-mode yasnippet undo-tree volatile-highlights counsel-projectile ace-window org-bullets counsel zygospore company helm-gtags helm-projectile helm-swoop helm use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 4.0)))))
