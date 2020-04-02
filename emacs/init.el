cf09870c13b6bc2bfcc15d32ae6e546b77286639
Jjlot <jingran84@163.com>
2020-04-02 13:42:36 +0800

    Modify font

--- emacs/init.el
+++ emacs/init.el
@@ -32,3 +32,12 @@
 ;; (require 'setup-ggtags)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 108 :width normal))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 4.0)))))
+
