9d39f76ad84fce60a84feb1fd73746d4505fa0ec
WalterRan <jingran84@163.com>
2023-08-24 20:22:24 +0800

    Leave the cat alone
    Signed-off-by: WalterRan <jingran84@163.com>

--- emacs/init.el
+++ emacs/init.el
@@ -1,43 +1,17 @@
-(setq url-proxy-services
-   '(("no_proxy" . "^\\(localhost\\|10.*\\)")
-     ("http" . "10.144.1.11:8080")
-     ("https" . "10.144.1.11:8080")))
-
 (require 'package)
 (add-to-list 'package-archives
-         '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
 
 (when (not package-archive-contents)
-    (package-refresh-contents))
+  (package-refresh-contents))
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
-
-;; Failed to verify signature archive-contents.sig:
-;; Looks like a bug in your version of Emacs.
 (setq package-check-signature nil)
 
-(add-to-list 'load-path "~/.emacs.d/custom")
-
-;; (require 'setup-general)
-;; (if (version< emacs-version "24.4")
-;;     (require 'setup-ivy-counsel)
-;;   (require 'setup-helm)
-;;   (require 'setup-helm-gtags))
-;; (require 'setup-ggtags)
-
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 108 :width normal))))
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 4.0)))))
-
