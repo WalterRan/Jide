20cd26aef1befb3eac7d03e11e0e0d815a2c68c3
WalterRan <jingran84@163.com>
2024-02-15 13:41:50 +0800

    Add some configurations for org mode
    Signed-off-by: WalterRan <jingran84@163.com>

--- emacs/init.el
+++ emacs/init.el
@@ -20,8 +20,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-export-html-postample nil)
+ '(org-hide-leading-stars t)
+ '(org-startup-indented nil)
  '(package-selected-packages
-   '(expand-region which-key auto-complete org-bullets counsel imenu-list nyan-mode)))
+   '(toc-org expand-region which-key auto-complete org-bullets counsel imenu-list nyan-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
