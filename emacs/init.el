e320ce60db15a33ba891e12f2b9be274f635d14e
WalterRan <jingran84@163.com>
2024-02-15 11:59:24 +0800

    Enable emacs to start with user-emacs-directory
    Signed-off-by: WalterRan <jingran84@163.com>

--- emacs/init.el
+++ emacs/init.el
@@ -14,4 +14,17 @@
 (setq use-package-always-ensure t)
 (setq package-check-signature nil)
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(org-babel-load-file (expand-file-name (concat user-emacs-directory "myinit.org")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(expand-region which-key auto-complete org-bullets counsel imenu-list nyan-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
