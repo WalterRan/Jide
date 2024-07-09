b5c97b09050f24e51930dea62d0a6de176922195
WalterRan <jingran84@163.com>
2024-07-09 15:37:51 +0800

    Update Zsh installation method
    Signed-off-by: WalterRan <jingran84@163.com>

--- install.sh
+++ install.sh
@@ -109,8 +109,7 @@ install_zsh() {
 
     rm ~/.oh-my-zsh -rf
 
-    curl -Lo /tmp/install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-    sh /tmp/install.sh
+    sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
 
     rm ~/.zshrc -rf
 
